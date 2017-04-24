7953cbf997a496b274d61079367035d1feb304ec
Jacky <jackyng46@yahoo.com>
2017-04-24 19:03:45 -0400

    Can add markers but lat error

--- frontend/components/map/routemap.jsx
+++ frontend/components/map/routemap.jsx
@@ -17,10 +17,10 @@ let _mapOptions = {
 };
 class RouteMap extends React.Component {
   //...
-  // constructor(props){
-  //   super(props);
-  //   this.addLatLng = this.addLatLng.bind(this);
-  // }
+  constructor(props){
+    super(props);
+    this.addLatLng = this.addLatLng.bind(this);
+  }
   componentDidMount() {
     // set the map to show SF
     const mapOptions = {
@@ -30,38 +30,38 @@ class RouteMap extends React.Component {
 
     // wrap the mapDOMNode in a Google Map
     this.map = new google.maps.Map(this.mapNode, mapOptions);
-    // this.drawRoute();
-    // this.map.addListener('click', this.addLatLng);
+    this.drawRoute();
+    this.map.addListener('click', this.addLatLng);
   }
 
-  // drawRoute(){
-  //   this.poly = new google.maps.Polyline({
-  //         strokeColor: '#000000',
-  //         strokeOpacity: 1.0,
-  //         strokeWeight: 3
-  //       });
-  //       this.poly.setMap(this.map);
-  // }
-  //
-  // addLatLng(event) {
-  //      var path = this.poly.getPath();
-  //
-  //      // Because path is an MVCArray, we can simply append a new coordinate
-  //      // and it will automatically appear.
-  //      path.push(event.latLng);
-  //
-  //      // Add a new marker at the new plotted point on the polyline.
-  //      var marker = new google.maps.Marker({
-  //        position: event.latLng,
-  //        title: '#' + path.getLength(),
-  //        map: this.map
-  //      });
-  //    }
+  drawRoute(){
+    this.poly = new google.maps.Polyline({
+          strokeColor: '#000000',
+          strokeOpacity: 1.0,
+          strokeWeight: 3
+        });
+        this.poly.setMap(this.map);
+  }
+
+  addLatLng(event) {
+       var path = this.poly.getPath();
+
+       // Because path is an MVCArray, we can simply append a new coordinate
+       // and it will automatically appear.
+       path.push(event.latLng);
+
+       // Add a new marker at the new plotted point on the polyline.
+       var marker = new google.maps.Marker({
+         position: event.latLng,
+         title: '#' + path.getLength(),
+         map: this.map
+       });
+     }
 
   render() {
     return (
       // ...
-      <div className = "GoogleMap" ref={ map => this.mapNode = map }/> // this ref gives us access to the map dom node
+      <div className = "GoogleMap" onClick ={this.addLatLng} ref={ map => this.mapNode = map }/> // this ref gives us access to the map dom node
       // ...
     )
   }
